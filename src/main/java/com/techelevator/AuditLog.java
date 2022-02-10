2d2926f61f4151102d52a5e0047afc15baa2bbbf
Britt Lawson <brittlawsonrn@gmail.com>
2022-02-10 18:31:28 -0500

    added to AuditLog class

--- src/main/java/com/techelevator/AuditLog.java
+++ src/main/java/com/techelevator/AuditLog.java
@@ -8,28 +8,40 @@ public class AuditLog {
 
     File auditFile = new File("Log.txt");
 
-    public void feedMoneyLogEntry (double cashInserted, double currentBalance) {
-        SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
-        String timeStamp = date.format(new Date());
+    public void feedMoneyLogEntry (double cashInserted, double updatedBalance) {
+        String timeStamp = getTimeStamp();
         try(FileOutputStream outputStream = new FileOutputStream(auditFile, true);
             PrintWriter writer = new PrintWriter(outputStream)) {
-            writer.println(">" + timeStamp + " FEED MONEY: $" + cashInserted + " $" + currentBalance);
+            writer.println(">" + timeStamp + " FEED MONEY: $" + cashInserted + " $" + updatedBalance);
         } catch (IOException e) {
             System.out.println("File not found.");
         }
     }
 
-    public void dispenseProductLogEntry (Product product, double currentBalance) {
-        SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
-        String timeStamp = date.format(new Date());
+    public void dispenseProductLogEntry (Product product, double updatedBalance) {
+        String timeStamp = getTimeStamp();
         try (FileOutputStream outputStream = new FileOutputStream(auditFile, true);
              PrintWriter writer = new PrintWriter(outputStream)) {
-            writer.println(">" + timeStamp + " " + product.getNameOfProduct() + " " + product.getSlotIdentifier() + " $" + (currentBalance + product.getPrice()) + " $" + currentBalance);
+            writer.println(">" + timeStamp + " " + product.getNameOfProduct() + " " + product.getSlotIdentifier() + " $" + (updatedBalance + product.getPrice()) + " $" + updatedBalance);
         } catch (IOException e) {
             System.out.println("File not found.");
         }
     }
 
-    
+    public void returnChangeLogEntry (double startingBalance) {
+        String timeStamp = getTimeStamp();
+        try (FileOutputStream outputStream = new FileOutputStream(auditFile, true);
+             PrintWriter writer = new PrintWriter(outputStream)) {
+            writer.println(">" + timeStamp + " GIVE CHANGE: $" + startingBalance + " $0.00");
+        } catch (IOException e) {
+            System.out.println("File not found.");
+        }
+    }
+
+    private String getTimeStamp () {
+        SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
+        String timeStamp = date.format(new Date());
+        return timeStamp;
+    }
 
 }

547868581bbaba6ab43513820b2590ad6f2770bb
Britt Lawson <brittlawsonrn@gmail.com>
2022-02-11 11:08:24 -0500

    corrected returnChange method and wrote unit tests for it

--- src/test/java/com/techelevator/VendingMachineTest.java
+++ src/test/java/com/techelevator/VendingMachineTest.java
@@ -27,6 +27,30 @@ public class VendingMachineTest {
         Assert.assertEquals("Munch Munch, Yum!", retrievedProduct.getDispenseMessage());
     }
 
+    @Test
+    public void returnChange_returns_correct_coins_given_1_15 () {
+        vendingMachine.addMoney(1.15);
+        String output = vendingMachine.returnChange();
+        String expected = "Your change is: $1.15. You receive: 4 quarter(s), 1 dime(s), and 1 nickel(s).";
+        Assert.assertEquals(expected, output);
+    }
+
+    @Test
+    public void returnChange_returns_correct_coins_given_5_20 () {
+        vendingMachine.addMoney(5.20);
+        String output = vendingMachine.returnChange();
+        String expected = "Your change is: $5.20. You receive: 20 quarter(s), 2 dime(s), and 0 nickel(s).";
+        Assert.assertEquals(expected, output);
+    }
+
+    @Test
+    public void returnChange_returns_correct_coins_given_0 () {
+        vendingMachine.addMoney(0);
+        String output = vendingMachine.returnChange();
+        String expected = "Your change is: $0.00. You receive: 0 quarter(s), 0 dime(s), and 0 nickel(s).";
+        Assert.assertEquals(expected, output);
+    }
+
 
 
 }
